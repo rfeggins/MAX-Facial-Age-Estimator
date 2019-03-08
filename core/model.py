1ede2eba4e2a3ec00e71a863ec1afb4e04cfa350
I-Hong Jhuo <ihjhuo@ibm.com>
2019-03-07 16:57:58 -0800

    resize for large image inputs

--- core/model.py
+++ core/model.py
@@ -32,6 +32,17 @@ def read_still_image(image_data):
         logger.error(e)
         abort(400, 'Invalid file type/extension. Please provide a valid image (supported formats: JPEG, PNG, TIFF).')
 
+def img_resize(input_data):
+    img_h, img_w, _ = np.shape(input_data)
+    if img_w > 1024:
+        ratio=1024/img_w
+        input_data=cv2.resize(input_data, (int(ratio*img_w), int(ratio*img_h)))
+    elif img_h > 1024:
+        ratio = 1024/img_h
+        input_data=cv2.resize(input_data, (int(ratio*img_w), int(ratio*img_h)))
+    return input_data, ratio
+
+
 class ModelWrapper(MAXModelWrapper):
     MODEL_META_DATA = {
         'id': 'ssrnet',
@@ -61,17 +72,20 @@ class ModelWrapper(MAXModelWrapper):
         # load pre-trained model
         self.model = SSR_net(self.img_size, self.stage_num, self.lambda_local, self.lambda_d)()
         self.model.load_weights(path)
-
         self.graph = tf.get_default_graph()
 
         logger.info('Loaded model')
 
     def _pre_process(self, input_img):
         ad = 0.4
-        img_h, img_w, _ = np.shape(input_img)
-        input_img = cv2.resize(input_img, (1024, int(1024 * img_h / img_w)))
+        ratio=1
         img_h, img_w, _ = np.shape(input_img)
 
+        # if image size > 1024 then resize
+        if img_h > 1024 or img_w > 1024:
+            input_img, ratio =img_resize(input_img)
+
+        img_h, img_w, _ = np.shape(input_img)
         detected = self.detector.detect_faces(input_img)
         faces = np.empty((len(detected), self.img_size, self.img_size, 3))
 
@@ -85,20 +99,24 @@ class ModelWrapper(MAXModelWrapper):
                 xw2 = min(int(x2 + ad * w), img_w - 1)
                 yw2 = min(int(y2 + ad * h), img_h - 1)
                 faces[i, :, :, :] = cv2.resize(input_img[yw1:yw2 + 1, xw1:xw2 + 1, :], (self.img_size, self.img_size))
-        return (faces, detected)
+        return (faces, detected, ratio)
 
     def _predict(self, pre_x):
         faces=pre_x[0]
         with self.graph.as_default():
             predicted_ages = self.model.predict(faces)
-        return (predicted_ages,pre_x[1])
+        return (predicted_ages,pre_x[1], pre_x[2])
 
     def _post_process(self,post_rst):
         predicted_ages=post_rst[0]
         detected=post_rst[1]
+        ratio=post_rst[2]
         pred_res = []
         for i, d in enumerate(detected):
             if d['confidence'] > 0.85:
                 pre_age=predicted_ages[i].astype(int)
+                if ratio!=1:
+                    ratio_box = [ int(x/ratio) for x in d['box']]
+                    d['box']=ratio_box
                 pred_res.append([{'box': d['box'], 'age':pre_age}])
         return pred_res
